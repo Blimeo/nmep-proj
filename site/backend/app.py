e22da9603e7bcb41b2e3eb2331c905dd29730eb9
Oliver Bryniarski <obryniarski@berkeley.edu>
2020-05-03 18:56:46 -0700

    website backend ml

--- site/backend/app.py
+++ site/backend/app.py
@@ -2,6 +2,16 @@ import flask as fsk
 import base64
 import re
 import cv2
+import imageio
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(os.path.dirname(currentdir))
+sys.path.insert(0,parentdir)
+import torch
+import torchvision.transforms as transforms
+from utils import *
+from models import *
+
 
 app = fsk.Flask(__name__)
 
@@ -22,6 +32,19 @@ def get_query_from_react():
     new_img = remove_transparency('original.png')
     new_img = cv2.resize(new_img, (32, 32), interpolation = cv2.INTER_AREA)
     cv2.imwrite('output.png', new_img)
+
+    # olivers edits - make sure this doesn't break anything
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = ResNet('18').to(device)
+    model.load_state_dict(torch.load('model'))
+    model.eval()
+    image = torch.Tensor(imageio.imread('output.png')).permute(2, 0, 1).to(device) / 255.
+    label = torch.Tensor([6]).to(device).long()
+    eps = 3500
+    steps = 100
+    # adv is the shifted image as a pytorch tensor which is (1, 3, 32, 32)
+    adv = pgd_attack(image.view(3,32,32), 6, model, stepsize=2.5 * eps / steps, eps=eps, steps=steps, constraint='l_2').cpu()
+
     return raw_image_data
 
 if __name__ == '__main__':
