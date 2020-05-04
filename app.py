f409f98b8df07f35a75f24f36530f5d61b206c2d
Matthew Ye <my1@berkeley.edu>
2020-05-04 18:44:26 -0400

    Dsafdfsadfsa

--- app.py
+++ app.py
@@ -55,4 +55,6 @@ def get_query_from_react():
     return b64_data
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
